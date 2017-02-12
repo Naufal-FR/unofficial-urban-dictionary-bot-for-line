e9a737afd3f006e901075f3b993e4c4d77ae8afc
Naufal-FR <amx.idproject28@gmail.com>
2017-02-12 12:16:18 +0700

    Added new function define_term and new command other

--- index.php
+++ index.php
@@ -14,6 +14,13 @@
 	}
 	set_error_handler('exceptions_error_handler');
 
+	function define_term ($term){
+		$term_url = 'http://api.urbandictionary.com/v0/define?term=' . str_replace(' ', '+', $term);
+		$term_json = file_get_contents($term_url);
+		$term_array = json_decode($term_json, true);
+		return $term_array ;
+	}
+
 	
 	$client = new LINEBotTiny($channelAccessToken, $channelSecret);
 
@@ -31,24 +38,41 @@
 	               		$exploded_Message = explode(" ", $message['text']);
 						
 						try {
+							
 							if ($exploded_Message[0] == "++define") {
 		                		$term = substr($message['text'], 9);
 
-								$term_url = 'http://api.urbandictionary.com/v0/define?term=' . str_replace(' ', '+', $term);
-
-								$term_json = file_get_contents($term_url);
-								$term_array = json_decode($term_json, true);
+		                		$term_array = define_term($term);
+								$no_result = count($term_array['list']);		
 
 								$word = 'Word : ' . $term_array['list'][0]['word'];
 								$definition = 'Definition : ' . $term_array['list'][0]['definition'];
 								$example = 'Example : ' . $term_array['list'][0]['example'];
+								$variation = 'There are ' . $no_result . ' variation for this definition.' . "\n" .'Type ++other <definition> to get a random variation' ;
+								$term_result_array = array ($word, $definition, $example, $variation);
 
-								$term_result_array = array ($word, $definition, $example);
+								$term_return = implode("\n\n",$term_result_array) . "";
+	                		} 
+
+	                		elseif ($exploded_Message[0] == "++other"){
+		                		$term = substr($message['text'], 8);
+
+		                		$term_array = define_term($term);
+								$no_result = count($term_array['list']);
+								
+								$lookup_value = rand(0,$no_result-1);
+
+								$word = 'Word : ' . $term_array['list'][$lookup_value]['word'];
+								$definition = 'Definition : ' . $term_array['list'][$lookup_value]['definition'];
+								$example = 'Example : ' . $term_array['list'][$lookup_value]['example'];
+								$variation = 'There are ' . $no_result . ' variation for this definition.' . "\n" . 'Type ++define <definition> to get the most voted variation' ;
+								$term_result_array = array ($word, $definition, $example, $variation);
 
 								$term_return = implode("\n\n",$term_result_array) . "";
 	                		}
+
 						} catch (Exception $e) {
-	                		$term_return = "Sorry,No Definition Found";	
+	                		$term_return = "Sorry, No Definition Found or Error Occured";	
 						}
 
 	                    $client->replyMessage(array(
